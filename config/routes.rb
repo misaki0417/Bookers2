0c85a1e8a080d9762b59a5decb07401a000b91f7
misaki0417 <110611390+misaki0417@users.noreply.github.com>
2022-10-08 14:29:11 +0000

    [add]課題チェックシート　footerのレイアウト作成　まで終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  root to: 'homes#top'
   devise_for :users
+  
+  resources :books ,only: [:index,:show]
+  resources :users ,only: [:index,:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
