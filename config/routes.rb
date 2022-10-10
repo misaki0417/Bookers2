9f6cbd37124a1c8f667af2f42f0c4a68089f24be
misaki0417 <110611390+misaki0417@users.noreply.github.com>
2022-10-10 14:51:12 +0000

    [add]課題チェックシート　sign upページの編集　まで終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
-  
+  get "homes/about"
   resources :books ,only: [:index,:show]
   resources :users ,only: [:index,:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
